e58c71b674570909fbd544e44375471234ea5707
lifangjie <lifangjie@chinayie.com>
2020-11-11 16:43:04 +0800

    格式化

--- foxnic-commons/src/main/java/com/github/foxnic/commons/xml/XML.java
+++ foxnic-commons/src/main/java/com/github/foxnic/commons/xml/XML.java
@@ -92,6 +92,7 @@ public class XML {
 		OutputFormat format = OutputFormat.createPrettyPrint();
 		format.setEncoding("UTF-8");
 		format.setIndentSize(4);
+		format.setSuppressDeclaration(false); 
 		try {
 			XMLWriter writer = new XMLWriter(new FileWriter(newPomFile), format);
 			writer.write(this.doc);
