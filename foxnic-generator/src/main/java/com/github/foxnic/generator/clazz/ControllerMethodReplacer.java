c170da475328f0bc7c59ee3dc6b017591332d19b
lifangjie <lifangjie@qq.com>
2021-03-25 20:08:51 +0800

    111

--- foxnic-generator/src/main/java/com/github/foxnic/generator/clazz/ControllerMethodReplacer.java
+++ foxnic-generator/src/main/java/com/github/foxnic/generator/clazz/ControllerMethodReplacer.java
@@ -3,6 +3,7 @@ package com.github.foxnic.generator.clazz;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.github.foxnic.commons.io.FileUtil;
@@ -12,6 +13,7 @@ import com.github.foxnic.commons.reflect.ReflectUtil;
 import com.github.foxnic.generator.CodePoint;
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import javassist.CtClass;
 import javassist.NotFoundException;
@@ -98,8 +100,10 @@ public class ControllerMethodReplacer {
 
 
 	public void replace(File sourceFile) throws Exception {
+		boolean isChanged=false;
 		readFile(sourceFile);
 		int i=findLineNumber("@ApiOperation","@ApiOperationSupport");
+		
 		if(i>0) {
 			ApiOperation ann=controllerMethod.getAnnotation(ApiOperation.class);
 			String location=controllerClass.getName()+"."+controllerMethod.getName()+"@ApiOperation.value";
@@ -108,22 +112,82 @@ public class ControllerMethodReplacer {
 			String oldcode=codePoint.getOldCode(location);
 			boolean edited=oldcode!=null && !oldcode.equals(current);
 			String newcode=codePoint.getNewCode(location);
-			newcode="XXX";
-			if(!edited) {
+			//如果未被编辑过，并且新代码与当前代码不一致，则替换
+			if(!edited && !newcode.equals(current) ) {
 				sourceLines[i] = replace(sourceLines[i], "value", "\""+current+"\"", "\""+newcode+"\"");
+				isChanged=true;
 			}
 		}
-		findLineNumber("@ApiImplicitParams");
-		System.out.println();
-		FileUtil.writeText(sourceFile, StringUtil.join(sourceLines,"\n"));
+		
+		//
+		i=findLineNumber("@ApiImplicitParams");
+		
+		for (int j = i+1; j < lineNumber && i>0 ; j++) {
+			boolean c=false;
+//			c=replaceApiImplicitParam(j,"value",true);
+//			if(c) {
+//				isChanged=true;
+//			}
+//			c=replaceApiImplicitParam(j,"required",false);
+//			if(c) {
+//				isChanged=true;
+//			}
+			c=replaceApiImplicitParam(j,"dataTypeClass",false);
+			if(c) {
+				isChanged=true;
+			}
+		}
+		
+		if(isChanged) {
+			FileUtil.writeText(sourceFile, StringUtil.join(sourceLines,"\n"));
+		}
 	}
 	
 	
+	private boolean replaceApiImplicitParam(int j, String key, boolean quate) {
+		String q="";
+		if(quate) q="\"";
+		boolean changed=false;
+		String line=sourceLines[j].trim();
+		if(!line.startsWith("@ApiImplicitParam")) return changed;
+		String name=getCurrent(line, "name");
+		String location=controllerClass.getName()+"."+controllerMethod.getName()+"@ApiImplicitParam."+name+"."+key;
+		String current=getCurrent(line, key);
+		String oldcode=q+codePoint.getOldCode(location)+q;
+		boolean edited=oldcode!=null && !oldcode.equals(current);
+		String newcode=q+codePoint.getNewCode(location)+q;
+		//如果未被编辑过，并且新代码与当前代码不一致，则替换
+		if(!edited && !newcode.equals(current)) {
+			line= replace(sourceLines[j], key, current, newcode);
+			sourceLines[j]=line;
+			changed=true;
+		}
+		return changed;
+	}
+
+
+
+
 	public String replace(String line,String key,String current,String newcode) {
 		int i=line.indexOf(key);
 		if(i==-1) return line;
 		i=line.indexOf(current, i+key.length());
 		line=line.substring(0,i)+newcode+line.substring(i+current.length());
+		return line;
+	}
+	
+	public String getCurrent(String line,String key) {
+		int i=line.indexOf(key);
+		if(i==-1) return line;
+		i=line.indexOf("=", i+key.length());
+		int j=line.indexOf(",", i+1);
+		int k=line.indexOf(")", i+1);
+		
+		if(j==-1 && k!=-1) {
+			j=k-1;
+		}
+		System.out.println();
+		line=line.substring(i+1,j).trim();
 		return line;
 	}
 	
