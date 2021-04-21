4c0f207237e5cfa3784c3c6d71c0ca2f2d2285b4
lifangjie <lifangjie@qq.com>
2021-04-21 08:58:29 +0800

    使用JoinPoint之前的备份

--- foxnic-dao/src/main/java/com/github/foxnic/dao/relation/JoinPoint.java
+++ foxnic-dao/src/main/java/com/github/foxnic/dao/relation/JoinPoint.java
@@ -1,5 +1,8 @@
 package com.github.foxnic.dao.relation;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.github.foxnic.sql.meta.DBField;
 import com.github.foxnic.sql.meta.DBTable;
 
@@ -7,30 +10,43 @@ public class JoinPoint {
 
 	private DBTable table;
 	private DBField[] fields;
+	private Set<String> fieldIds;
 	
 	public JoinPoint(DBField... field) {
 		if(field==null || field.length==0) {
 			throw new IllegalArgumentException("JoinPoint 至少需要一个字段");
 		}
+		fieldIds=new HashSet<>();
 		for (DBField f : field) {
+			
+			//检查重复的字段
+			if(fieldIds.contains(f.getId())) {
+				throw new IllegalArgumentException("JoinPoint 不允许同名字段 "+f.table().name()+"."+f.name());
+			}
+			
+			fieldIds.add(f.getId());
+			//设置表
 			if(table==null) {
 				table=f.table();
 				continue;
 			}
-			//
+			//校验表
 			if(!table.name().equalsIgnoreCase(f.table().name())) {
 				throw new IllegalArgumentException("JoinPoint 表名不一致 : "+table.name()+","+f.table().name());
 			}
 		}
 	}
 	
+	/**
+	 * 检查两个 JoinPoint 是否匹配
+	 * */
 	public boolean match(JoinPoint joinPoint) {
 		//如果表名不一致，则不匹配
 		if(!joinPoint.table.name().equalsIgnoreCase(this.table.name())) return false;
 		//如果字段数量不一致，则不匹配
 		if(joinPoint.fields.length!=this.fields.length) return false;
 		for (DBField f : this.fields) {
-			
+			if(!this.fieldIds.contains(f.getId())) return false;
 		}
 		return true;
 	}
--- foxnic-sql/src/main/java/com/github/foxnic/sql/meta/DBField.java
+++ foxnic-sql/src/main/java/com/github/foxnic/sql/meta/DBField.java
@@ -7,12 +7,14 @@ public class DBField {
 	private String var;
 	private String label;
 	private String detail;
+	private String id;
 	
 	public DBField(String name,String var,String label,String detail) {
 		this.name=name;
 		this.label=label;
 		this.detail=detail;
 		this.var=var;
+		this.id=(this.table+"."+this.name).toLowerCase();
 	}
 	
 	public DBTable table() {
@@ -43,6 +45,13 @@ public class DBField {
 	@Override
 	public String toString() {
 		return this.name;
+	}
+
+	/**
+	 * 表名与字段名的拼接，大写，形式如： SYS_USER.NAME
+	 * */
+	public String getId() {
+		return id;
 	}
 	
 }
