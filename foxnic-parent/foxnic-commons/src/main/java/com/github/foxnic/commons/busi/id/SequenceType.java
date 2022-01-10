9eed97d91379dd08e24bf9ffc7af7675202355ee
lifangjie <leefangjie@qq.com>
2022-01-10 11:12:44 +0800

    修复序列在日期切换时还有残余序号的问题

--- foxnic-parent/foxnic-commons/src/main/java/com/github/foxnic/commons/busi/id/SequenceType.java
+++ foxnic-parent/foxnic-commons/src/main/java/com/github/foxnic/commons/busi/id/SequenceType.java
@@ -9,31 +9,38 @@ public enum SequenceType implements CodeTextEnum {
 	/**
 	 * 序号自增
 	 * */
-	AI("自增"), 
+	AI("自增",null),
 	/**
 	 * 每日自增  如 20180208006
 	 * */
-	DAI("每日自增"), 
-	
+	DAI("每日自增","yyyyMMdd"),
+
 	/**
 	 * 一年的第几周，
 	 * 可以有不同的定义，具体业务系统可以有不同的解释与实现
 	 * */
-	WAI_1("每周自增"), 
+	WAI_1("每周自增","yyyyww"),
 	/**
 	 * 一年的第几周，
 	 * 可以有不同的定义，具体业务系统可以有不同的解释与实现
 	 * */
-	WAI_2("每周自增"),
+	WAI_2("每周自增","yyyyww"),
 	/**
 	 * 每月自增  如 201801
 	 * */
-	MAI("每月自增"), 
+	MAI("每月自增","yyyyMM"),
 	/**
 	 * 年自增 如 201901
 	 * */
-	YAI("每年自增");
+	YAI("每年自增","yyyy");
+
     private String text=null;
+
+	public String dateTagFormat() {
+		return dateTagFormat;
+	}
+
+	private String dateTagFormat=null;
     /**
 	 * @return the text
 	 */
@@ -41,8 +48,9 @@ public enum SequenceType implements CodeTextEnum {
 		return text;
 	}
 
-	private SequenceType(String text)
+	private SequenceType(String text,String dateTagFormat)
     {
+    	this.dateTagFormat=dateTagFormat;
         this.text=text;
     }
 
--- foxnic-parent/foxnic-dao/src/main/java/com/github/foxnic/dao/spec/DBSequence.java
+++ foxnic-parent/foxnic-dao/src/main/java/com/github/foxnic/dao/spec/DBSequence.java
@@ -3,6 +3,7 @@ package com.github.foxnic.dao.spec;
 import com.github.foxnic.commons.busi.id.IDGenerator;
 import com.github.foxnic.commons.busi.id.SequenceType;
 import com.github.foxnic.commons.collection.MapUtil;
+import com.github.foxnic.commons.lang.DateUtil;
 import com.github.foxnic.commons.lang.StringUtil;
 import com.github.foxnic.commons.log.Logger;
 import com.github.foxnic.dao.data.Rcd;
@@ -19,9 +20,11 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class DBSequence {
 
-    private static final Set<String> sequenceIds=new HashSet<>();
-    private static final ConcurrentHashMap<String,Integer> sequenceFetchSize=new ConcurrentHashMap<String,Integer>();
-    private static final ConcurrentHashMap<String,ConcurrentLinkedQueue<String>> sequencesPool=new ConcurrentHashMap<String,ConcurrentLinkedQueue<String>>();
+    private static final Set<String> SEQUENCE_IDS =new HashSet<>();
+    private static final ConcurrentHashMap<String,Integer> SEQUENCE_FETCH_SIZE =new ConcurrentHashMap<String,Integer>();
+    private static final ConcurrentHashMap<String,SequenceType> SEQUENCE_TYPES =new ConcurrentHashMap<String,SequenceType>();
+    private static final ConcurrentHashMap<String,String> DATE_TAGS =new ConcurrentHashMap<String,String>();
+    private static final ConcurrentHashMap<String,ConcurrentLinkedQueue<String>> SEQUENCES_POOL =new ConcurrentHashMap<String,ConcurrentLinkedQueue<String>>();
 
 
     private DAO dao;
@@ -43,12 +46,17 @@ public class DBSequence {
             this.tenantId = "defaults";
         }
         this.key=dao.getDbIdentity()+"."+tenantId+"."+id;
+        SequenceType sequenceType= SEQUENCE_TYPES.get(this.key);
 
-        Integer fetchSize=sequenceFetchSize.get(key);
-        if(fetchSize==null) {
-            fetchSize=dao.queryInteger("select fetch_size from "+table+" where id=? and tenant_id=?",id,this.tenantId);
+        Integer fetchSize= SEQUENCE_FETCH_SIZE.get(key);
+
+        if(fetchSize==null || sequenceType==null) {
+            Rcd r=dao.queryRecord("select fetch_size,type from "+table+" where id=? and tenant_id=?",id,this.tenantId);
+            fetchSize=r.getInteger("fetch_size");
+            sequenceType=r.getEnum("type",SequenceType.class,null,"name");
             if(fetchSize==null) fetchSize=4;
-            sequenceFetchSize.put(key,fetchSize);
+            SEQUENCE_FETCH_SIZE.put(key,fetchSize);
+            SEQUENCE_TYPES.put(key,sequenceType);
         }
 
 
@@ -124,23 +132,23 @@ public class DBSequence {
      * 判断是否已经存在
      * */
     public boolean exists() {
-        if (sequenceIds.contains(key)) {
+        if (SEQUENCE_IDS.contains(key)) {
             return true;
         }
         boolean ex=existInTable();
         if (ex) {
-            sequenceIds.add(key);
+            SEQUENCE_IDS.add(key);
         }
         return ex;
     }
 
     public void setFetchSize(int size) {
-        sequenceFetchSize.put(key, size);
+        SEQUENCE_FETCH_SIZE.put(key, size);
         dao.execute("update "+table+" set fetch_size=? where id=? and tenant_id=?",size,id,this.tenantId);
     }
 
     public Integer getFetchSize() {
-        Integer size=sequenceFetchSize.get(key);
+        Integer size= SEQUENCE_FETCH_SIZE.get(key);
         if(size==null) {
             return 4;
         }
@@ -153,14 +161,28 @@ public class DBSequence {
      * @return 序列值
      */
     public String next() {
-        ConcurrentLinkedQueue<String> queue=sequencesPool.get(key);
+        ConcurrentLinkedQueue<String> queue= SEQUENCES_POOL.get(key);
         if(queue==null) {
             queue = new ConcurrentLinkedQueue<String>();
-            sequencesPool.put(key,queue);
+            SEQUENCES_POOL.put(key,queue);
         }
         String val=null;
+        String dateTag=null;
+        SequenceType sequenceType= SEQUENCE_TYPES.get(this.key);
+        if(sequenceType.dateTagFormat()!=null) {
+            dateTag=DateUtil.getCurrTime(sequenceType.dateTagFormat());
+        }
+        String currDateTag= DATE_TAGS.get(this.key);
         try {
-            val=queue.remove();
+            if(currDateTag!=null && dateTag!=null) {
+                if(currDateTag.equals(dateTag)) {
+                    val = queue.remove();
+                } else {
+                    queue.clear();
+                }
+            } else {
+                val = queue.remove();
+            }
         } catch (NoSuchElementException e) {
             val=null;
         }
@@ -181,7 +203,7 @@ public class DBSequence {
             Rcd r=dao.queryRecord("select * from "+table+" where id=? and tenant_id=?",this.id,this.tenantId);
             if(r==null) {
                 if(createTenantCopy()) {
-                    sequenceIds.remove(key);
+                    SEQUENCE_IDS.remove(key);
                     fetchSequenceValues(quene);
                     return;
                 } else {
@@ -199,9 +221,14 @@ public class DBSequence {
             p.declareOutParameter("sval", Types.VARCHAR);
             Map<String, Object> ret = p.execute(MapUtil.asStringKeyMap("id", id,"tid",tenantId,"num",size));
             String vals=ret.get("sval") + "";
+            SequenceType sequenceType= SEQUENCE_TYPES.get(this.key);
+            if(sequenceType.dateTagFormat()!=null) {
+                DATE_TAGS.put(this.key, DateUtil.getCurrTime(sequenceType.dateTagFormat()));
+            }
+            // 如果没有渠道序列值
             if(StringUtil.isEmpty(vals)) {
                 if(!existInTable()){
-                    sequenceIds.remove(key);
+                    SEQUENCE_IDS.remove(key);
                     createTenantCopy();
                     fetchSequenceValues(quene);
                     return;
@@ -209,6 +236,7 @@ public class DBSequence {
                     throw new RuntimeException("获取序列失败");
                 }
             }
+
             String[] valArr=vals.split(",");
             for (String val : valArr) {
                 if(val==null || val.length()==0) {
