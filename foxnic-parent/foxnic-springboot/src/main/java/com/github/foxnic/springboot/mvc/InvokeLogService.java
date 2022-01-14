6b4cccf6f147d16ebe55c33b97bdac4dfd987ce6
lifangjie <leefangjie@qq.com>
2022-01-14 16:48:40 +0800

    增加接口调用的日志输出

--- foxnic-parent/foxnic-springboot/src/main/java/com/github/foxnic/springboot/mvc/InvokeLogService.java
+++ foxnic-parent/foxnic-springboot/src/main/java/com/github/foxnic/springboot/mvc/InvokeLogService.java
@@ -1,12 +1,16 @@
 package com.github.foxnic.springboot.mvc;
 
+import java.lang.reflect.Method;
+
 public interface InvokeLogService {
-	
+
+	void logRequest(Method method,String uri, String url,String body);
+
 	void start(RequestParameter request);
-	
+
 	void exception(Throwable error);
-	
+
 	void response(Object response);
 
-	
+
 }
