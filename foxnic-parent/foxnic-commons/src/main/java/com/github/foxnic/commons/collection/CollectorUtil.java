779df3bc9cab03bf722bb5cea0907a81519a88bb
lifangjie <leefangjie@qq.com>
2022-04-29 17:34:37 +0800

    流程待办&审批10%

--- foxnic-parent/foxnic-commons/src/main/java/com/github/foxnic/commons/collection/CollectorUtil.java
+++ foxnic-parent/foxnic-commons/src/main/java/com/github/foxnic/commons/collection/CollectorUtil.java
@@ -110,7 +110,17 @@ public class CollectorUtil {
 		 return list;
 	}
 
-
+	/**
+	 * 将 S 的集合转为 T 的集合
+	 * */
+	public static <S,T> List<T> castList(List<S> list,DataCreateHandler<S,T> handler) {
+		List<T> result=new ArrayList<>();
+		for (S s : list) {
+			T t=handler.handle(s);
+			result.add(t);
+		}
+		return result;
+	}
 
 
 	public static  interface DataUpdateHandler<S,T> {
