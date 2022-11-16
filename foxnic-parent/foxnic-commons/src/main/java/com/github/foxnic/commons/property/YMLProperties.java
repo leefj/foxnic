fcf3e15996b6b2b924229de522861ff8686b09ab
lifangjie <leefangjie@qq.com>
2022-11-16 13:31:49 +0800

    增加离散值保存、查询、删除的特性

--- foxnic-parent/foxnic-commons/src/main/java/com/github/foxnic/commons/property/YMLProperties.java
+++ foxnic-parent/foxnic-commons/src/main/java/com/github/foxnic/commons/property/YMLProperties.java
@@ -1,30 +1,33 @@
 package com.github.foxnic.commons.property;
 
+import com.github.foxnic.commons.cache.Variable;
+import org.yaml.snakeyaml.Yaml;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.yaml.snakeyaml.Yaml;
-
-import com.github.foxnic.commons.cache.Variable;
-
 /**
  * 从YML读取配置信息
  * */
 public class YMLProperties {
-	
+
 	private Map<String,Object> data = null;
-	
+
 	public YMLProperties()
 	{
 		data = new HashMap<String, Object>();
 	}
+
+	private String filePath;
+
 	/**
 	 * @param path 文件路径
 	 * */
 	public YMLProperties(String path)
 	{
+		this.filePath = path;
 		Yaml yaml = new Yaml();
         try {
         	data = (Map)yaml.loadAs(new FileInputStream(path),Map.class);
@@ -32,13 +35,15 @@ public class YMLProperties {
             throw new RuntimeException("文件读取异常："+path,e);
         }
 	}
-	
+
 	public YMLProperties(File file) {
 		this(file.getAbsolutePath());
 	}
-	
-	
-	
+
+	public String geFilePath() {
+		return filePath;
+	}
+
 	/**
 	 * 获得属性值
 	 * @param name 属性名，层级间用小数点隔开
@@ -57,7 +62,7 @@ public class YMLProperties {
 		return new Variable(value);
 	}
 
-	
+
 	private Object readProperty(Map<String,Object> map,String[] sp, int i) {
 		if(i>=sp.length) return null;
 		Object value=map.get(sp[i]);
@@ -70,5 +75,5 @@ public class YMLProperties {
 			return value;
 		}
 	}
-	
+
 }
